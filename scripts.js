d5f73096f99ee1f9ca9b7a0dd8c169f6c1149717
Beth Presten <bethpresten@gmail.com>
2020-10-17 11:14:17 -0400

    dynamically populated div is populating with dummy data

--- scripts.js
+++ scripts.js
@@ -72,15 +72,44 @@ $(document).ready(function () {
 
 
 
-    for (var i = 0; i < 5; i++) {
-        var resultsDiv = $("<div>").addClass("card bg-light mb-3");
-        resultsDiv.attr("style", "max-width: 18rem")
-        var physicianName = $("h5>");
-        var physicianAddress = $("<span>");
-        var physicianPhoneNumber = $("<span>");
-        var physicianDirectionsLink = $("<span>");
-        var covidRisk = $("<span>");
-        resultsDiv = append(physicianName, physicianAddress, physicianPhoneNumber, physicianDirectionsLink, covidRisk)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    for (var i = 0; i < 5; i++) {
+        var resultsDiv = $(".results-div")
+        var cardDiv = $("<div>").addClass("card bg-light mb-3");
+        cardDiv.attr("style", "max-width: 18rem");
+        var physicianName = $("<h5>").text("Dr. Michael Johnson");
+        var physicianAddress = $("<span>").text("555 1st Street");
+        var physicianPhoneNumber = $("<span>").text("555-555-5555");
+        var physicianDirectionsLink = $("<span>").text("click here for directions").attr("src", "http://maps.google.com");
+        var covidRisk = $("<span>").text("COVID Risk: low");
+        cardDiv.append(physicianName, physicianAddress, physicianPhoneNumber, physicianDirectionsLink, covidRisk)
+        resultsDiv.append(cardDiv);
     }
-}
+})
