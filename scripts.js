46a9787742c9b64da9ff1c1bc4a5196dd2eb3413
Beth Presten <bethpresten@gmail.com>
2020-10-17 10:21:45 -0400

    changed the hard coded results div to a dynamically created results-div in the script.js sheet

--- scripts.js
+++ scripts.js
@@ -1,2 +1,86 @@
 console.log("JS is working!");
 
+$(document).ready(function () {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    for (var i = 0; i < 5; i++) {
+        var resultsDiv = $("<div>").addClass("card bg-light mb-3");
+        resultsDiv.attr("style", "max-width: 18rem")
+        var physicianName = $("h5>");
+        var physicianAddress = $("<span>");
+        var physicianPhoneNumber = $("<span>");
+        var physicianDirectionsLink = $("<span>");
+        var covidRisk = $("<span>");
+        resultsDiv = append(physicianName, physicianAddress, physicianPhoneNumber, physicianDirectionsLink, covidRisk)
+
+    }
+}
