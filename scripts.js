943a5ba026473ee2240516e4fe90148fcf360f21
Joseph Strickland <jks2090@outlook.com>
2020-10-19 20:00:05 -0400

    added scripts

--- scripts.js
+++ scripts.js
@@ -1,35 +1,164 @@
+
+var stateInput;
+var stateCode;
+var stateList = {
+	'Arizona': 'AZ',
+	'Alabama': 'AL',
+	'Alaska':'AK',
+	'Arkansas': 'AR',
+	'California': 'CA',
+	'Colorado': 'CO',
+	'Connecticut': 'CT',
+	'Delaware': 'DE',
+	'Florida': 'FL',
+	'Georgia': 'GA',
+	'Hawaii': 'HI',
+	'Idaho': 'ID',
+	'Illinois': 'IL',
+	'Indiana': 'IN',
+	'Iowa': 'IA',
+	'Kansas': 'KS',
+	'Kentucky': 'KY',
+	'Louisiana': 'LA',
+	'Maine': 'ME',
+	'Maryland': 'MD',
+	'Massachusetts': 'MA',
+	'Michigan': 'MI',
+	'Minnesota': 'MN',
+	'Mississippi': 'MS',
+	'Missouri': 'MO',
+	'Montana': 'MT',
+	'Nebraska': 'NE',
+	'Nevada': 'NV',
+	'New Hampshire': 'NH',
+	'New Jersey': 'NJ',
+	'New Mexico': 'NM',
+	'New York': 'NY',
+	'North Carolina': 'NC',
+	'North Dakota': 'ND',
+	'Ohio': 'OH',
+	'Oklahoma': 'OK',
+	'Oregon': 'OR',
+	'Pennsylvania': 'PA',
+	'Rhode Island': 'RI',
+	'South Carolina': 'SC',
+	'South Dakota': 'SD',
+	'Tennessee': 'TN',
+	'Texas': 'TX',
+	'Utah': 'UT',
+	'Vermont': 'VT',
+	'Virginia': 'VA',
+	'Washington': 'WA',
+	'West Virginia': 'WV',
+	'Wisconsin': 'WI',
+	'Wyoming': 'WY'
+}
+
+
 console.log("JS is working!");
+
 
 var cityInput;
 function searchCity() {
-    try {
-        var cityStorage = localStorage.getItem("city");
-        var registryURL = "https://cors-anywhere.herokuapp.com/https://npiregistry.cms.hhs.gov/api/?version=2.1&address_purpose=primary&city=+ &address_purpose=primary&city=" + cityStorage;
-        $.ajax({
-            url: registryURL,
-            method: "GET"
-        }).then(function (responseRegistry) {
-            console.log(responseRegistry);
-            for (var i = 0; i < 10; i++) {
-                console.log("Name: " + responseRegistry.results[i].basic.last_name + ", " + responseRegistry.results[i].basic.first_name);
-                console.log("Gender: " + responseRegistry.results[i].basic.gender);
-                console.log("Address: " + responseRegistry.results[i].addresses[0].address_1 + " " + responseRegistry.results[i].addresses[0].address_2 + ", " + responseRegistry.results[i].addresses[0].city + ", " + responseRegistry.results[i].addresses[0].state);
-                console.log("Telephone: " + responseRegistry.results[i].addresses[0].telephone_number);
-                console.log("Postal Code: " + responseRegistry.results[i].addresses[0].postal_code);
-            }
-        });
-    } 
-    catch (error) {
-        console.log(error);
-    }
+	var stateStorage = localStorage.getItem("state");
+	console.log(stateStorage);
+	stateInput = localStorage.getItem("state");
+	console.log(stateInput);
+	getStateTwoDigitCode = function (stateInput) {
+	 stateCode = this.stateList[stateInput]; 
+	return stateCode;
+	}
+	
+	
+console.log(stateCode);		
+	const settingsR = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://rapidapi.p.rapidapi.com/reports?region_province=" +stateStorage+ "&iso=USA&region_name=US",
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
+			"x-rapidapi-key": "4e7288d871msh4411859255a7ef9p15aabejsn6bf06bbee388"
+		}
+	};
+
+	$.ajax(settingsR).done(function (responseR) {
+		console.log(responseR);
+		
+		var newCases = responseR.data[0].region.cities[0].confirmed_diff;
+		var riskLevel = "high"
+
+		//COVID RISK LEVEL DETERMINATION
+		// For new case count at a location: < 250 = level 1 (low), 251 to 500 = level 2 (moderate), >501 = level 3 (high) > lat: "33.94243204" long: "-84.5761255"
+		if (newCases < 250) {
+			var riskLevel = "low";
+		} else if (newCases >= 251 && newCases <= 500) {
+			var riskLevel = "moderate";
+		}
+
+		console.log("Covid risk level for county: " + riskLevel);
+		try {
+			var cityStorage = localStorage.getItem("city");
+			cityStorage = cityStorage.split(" ").join("+");
+			
+			var registryURL = "https://cors-anywhere.herokuapp.com/https://npiregistry.cms.hhs.gov/api/?version=2.1&address_purpose=primary&city=+ &address_purpose=primary&city=" + cityStorage;
+			$.ajax({
+				url: registryURL,
+				method: "GET"
+			}).then(function (responseRegistry) {
+				console.log(responseRegistry);
+
+				for (var i = 0; i < 10; i++) {
+
+					
+
+					if (responseRegistry.result_count == 0) {
+						var resultsDiv = $(".results-div")
+						var cardDiv = $("<div>").addClass("card bg-light mb-3");
+						cardDiv.attr("style", "max-width: 18rem");
+						var physicianName = $("<h5>").text("No results found, try again");
+						cardDiv.append(physicianName)
+						resultsDiv.append(cardDiv);
+						i = 10;
+					}
+					else if (typeof responseRegistry.results[i].basic.last_name == 'undefined') {
+						console.log("undefined")
+					}
+					else {
+						var resultsDiv = $(".results-div")
+						var cardDiv = $("<div>").addClass("card bg-light mb-3");
+						cardDiv.attr("style", "max-width: 18rem");
+						var physicianName = $("<h5>").text(responseRegistry.results[i].basic.last_name + ", " + responseRegistry.results[i].basic.first_name);
+						var physicianAddress = $("<span>").text(responseRegistry.results[i].addresses[0].address_1 + " " + responseRegistry.results[i].addresses[0].address_2 + ", " + responseRegistry.results[i].addresses[0].city + ", " + responseRegistry.results[i].addresses[0].state);
+						var physicianPhoneNumber = $("<span>").text(responseRegistry.results[i].addresses[0].telephone_number);
+						var physicianDirectionsLink = $("<span>").text("click here for directions").attr("src", "http://maps.google.com");
+						var covidRisk = $("<span>").text("COVID Risk for " +stateStorage + ": " + riskLevel);
+						cardDiv.append(physicianName, physicianAddress, physicianPhoneNumber, physicianDirectionsLink, covidRisk)
+						resultsDiv.append(cardDiv);
+
+					}
+
+				}
+			});
+		}
+		catch (error) {
+			console.log(error);
+		}
+
+	})
+
+
 };
 
 $("#submitBtn").on("click", function (event) {
-    event.preventDefault();
-    cityInput = $("#city-name").val();
-    localStorage.setItem("city", cityInput);
-    console.log(cityInput);
-    window.location.replace("./results.html");
+	event.preventDefault();
+	stateInput =$("#state-name").val();
+	localStorage.setItem("state", stateInput)
+	cityInput = $("#city-name").val();
+	localStorage.setItem("city", cityInput);
+	console.log(cityInput);
+	window.location.replace("./results.html");
+
 });
 
 searchCity();
@@ -95,55 +224,3 @@ console.log("loaded");
 
 
 
-
-
-
-
-
-//COVID reports
-const settingsR = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://rapidapi.p.rapidapi.com/reports?region_province=Georgia&iso=USA&region_name=US&city_name=gwinnett&date=2020-04-16&q=US%20Georgia",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "covid-19-statistics.p.rapidapi.com",
-		"x-rapidapi-key": "4e7288d871msh4411859255a7ef9p15aabejsn6bf06bbee388"
-	}
-};
-
-$.ajax(settingsR).done(function (responseR) {
-	console.log(responseR);
-	console.log("County: " + responseR.data[0].region.cities[0].name);
-	console.log("new cases = " + responseR.data[0].region.cities[0].confirmed_diff);
-	console.log("County latitude = " + responseR.data[0].region.cities[0].lat);
-	console.log("County longitude = " + responseR.data[0].region.cities[0].long);
-	var newCases = responseR.data[0].region.cities[0].confirmed_diff;
-	var riskLevel = "high"
-
-	//COVID RISK LEVEL DETERMINATION
-	// For new case count at a location: < 250 = level 1 (low), 251 to 500 = level 2 (moderate), >501 = level 3 (high) > lat: "33.94243204" long: "-84.5761255"
-	if (newCases < 250) {
-		var riskLevel = "low";
-	} else if (newCases >= 251 && newCases <= 500) {
-		var riskLevel = "moderate";
-	}
-
-	console.log("Covid risk level for county: " + riskLevel);
-	for (var i = 0; i < 5; i++) {
-		var resultsDiv = $(".results-div")
-		var cardDiv = $("<div>").addClass("card bg-light mb-3");
-		cardDiv.attr("style", "max-width: 18rem");
-		var physicianName = $("<h5>").text("Dr. Michael Johnson");
-		var physicianAddress = $("<span>").text("555 1st Street");
-		var physicianPhoneNumber = $("<span>").text("555-555-5555");
-		var physicianDirectionsLink = $("<span>").text("click here for directions").attr("src", "http://maps.google.com");
-		var covidRisk = $("<span>").text("COVID Risk: low");
-		cardDiv.append(physicianName, physicianAddress, physicianPhoneNumber, physicianDirectionsLink, covidRisk)
-		resultsDiv.append(cardDiv);
-	}
-
-
-})
-
-// var cityName = localStorage.getItem("city-name");
